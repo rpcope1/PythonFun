43a8bb726aec96750fb18f8e817f35682402be50
rpcope1 <rpcope1@gmail.com>
2014-09-08 21:20:04 -0600

    Cleaned up some of the way the gls was parsed. Still thinking about header parsing. Added a basic integration test for your functions, so that you can compare old data against any new changes, as you make them.

--- IncludeSwitch.py
+++ IncludeSwitch.py
@@ -3,11 +3,23 @@ import os
 from collections import defaultdict
 #Use sys to get args.
 import sys
+#Use regex to find stuff in the file.as
+import re
 
 LIBRARY_DIRECTORY = "."
 #Let's use a set literal.
 EXTENSIONS = {".gls", ".h"}
 
+TEMP_GLS_NAME = "temp.gls_"
+TEMP_HEADER_NAME = "temp.h_"
+
+#Use this template with basename substituted in using the format function to generate a regex for each file.
+INCLUDE_HEADER_REGEX_TEMPLATE = "\s*#include\s+((<\s*{basename}.h\s*>)|(\"\s*{basename}.h\s*\"))"
+INCLUDE_GLOBAL_LIB_REGEX_TEMPLATE = "\s*#include\s+((<\s*{basename}.gls\s*>)|(\"\s*{basename}.gls\s*\"))"
+INCLUDE_ALL_GLOBALS_REGEX = "\s*#include\s+((<\s*[^>]+.gls\s*>)|(\"\s*[^\"]+.gls\s*\"))"
+#TODO: Maybe we don't want to alter #includes in comments?
+
+FS_SEPERATOR = "\\" if os.name in ['nt', 'os2'] else "/"  # If os.name is posix, java or riscos, use unix separator
 
 def get_common_elements(path, extensions):
     """
@@ -23,24 +35,35 @@ def get_common_elements(path, extensions):
     return [file_basename for file_basename, file_exts in file_basename_ext_dict.items() if file_exts == extensions]
 
 
-def modify_includes(file_path, file_name):
-    file_path = "".join([file_path, "/", file_name, ".gls"])
+#Note: I tacked on the _ at the end so these don't get confused with existing files.
+def modify_includes(dir_path, file_basename, new_global_libary_file="temp.gls_", new_header_file="temp.h_"):
+    """
+        For a given directory path (dir_path) and file_basename (file name without the extension),
+        attempt to write new temporary files with the includes in the correct spot.
+    """
+    #TODO: Check to ensure this function now works correctly with #ifdef #endif for multiple includes in scripts.
+    #TODO: Ensure all preprocessor directives for gls are inserted at head, and directives are inserted correctly
+    #TODO: in correct place in header file, even if they weren't before.
+    #This should work on all platforms now.
+    file_path = FS_SEPERATOR.join([dir_path, "{}.gls".format(file_basename)])
     file_buffer = []
-    with open(file_path, "r") as global_file:
-        for line in global_file:
-            if "#include" and file_name in line:
+    with open(file_path, "r") as current_file:
+        current_include_header_regex = INCLUDE_HEADER_REGEX_TEMPLATE.format(basename=file_basename)
+        for line in current_file:
+            #if "#include" and file_basename in line:  # Should be if "#include" in line and file_basename in line.
+            #That doesn't do what you think it should. Regex is probably good here.
+            if re.search(current_include_header_regex, line):
                 file_buffer.append("\n")
-            elif "#include" and ".gls" in line:
+            elif re.search(INCLUDE_ALL_GLOBALS_REGEX, line):
                 global_to_header = line.partition(".gls")
                 file_buffer.append(global_to_header[0] + ".h" + global_to_header[2])
             else:
                 file_buffer.append(line)
 
-    with open("temp1", "w") as output_gls:
-        for line in file_buffer:
-            output_gls.write(line)
+    with open(new_global_libary_file, "w") as output_gls:
+        output_gls.write("".join(file_buffer))
 
-    file_path = file_path[:-4] + ".h"
+    file_path = FS_SEPERATOR.join([dir_path, "{}.h".format(file_basename)])
     file_buffer = []
 
     with open(file_path, "r") as global_file:
@@ -48,17 +71,17 @@ def modify_includes(file_path, file_name):
         for line in global_file:
             file_buffer.append(line)
             if "#endif" in line:
-                count_end_if = count_end_if + 1
+                count_end_if += 1
         number_of_end_if = count_end_if
         count_end_if = 0
 
-    with open("temp2", "w") as output_h:
+    with open(new_header_file, "w") as output_h:
         for line in file_buffer:
             if "#endif" in line:
                 count_end_if += 1
                 if count_end_if == number_of_end_if:
                     print "success"
-                    output_h.write('#include "' + file_name + '.gls\"\n' + line)
+                    output_h.write('#include "' + file_basename + '.gls\"\n' + line)
                 else:
                     output_h.write(line)
             else:
--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-__author__ = 'rcope'
+#TODO: Make this into a true unit test implementation.
+import IncludeSwitch
+import hashlib
+
+IncludeSwitch.modify_codebase_includes(".", IncludeSwitch.EXTENSIONS)
+
+
+
+with open("test_ref.gls_") as f:
+    contents = f.read().strip()
+    test_ref_gls_hash_digest = hashlib.md5(contents).digest()
+
+with open(IncludeSwitch.TEMP_GLS_NAME) as f:
+    contents = f.read().strip()
+    temp_gls_hash_digest = hashlib.md5(contents).digest()
+
+with open("test_ref.h_") as f:
+    contents = f.read().strip()
+    test_ref_h_hash_digest = hashlib.md5(contents).digest()
+
+with open(IncludeSwitch.TEMP_HEADER_NAME) as f:
+    contents = f.read().strip()
+    temp_h_hash_digest = hashlib.md5(contents).digest()
+
+print "TESTING..."
+print "GLS COMPARE TEST:",  "PASS" if test_ref_gls_hash_digest == temp_gls_hash_digest else "FAIL"
+print "HEADER COMPARE TEST:",  "PASS" if test_ref_h_hash_digest == temp_h_hash_digest else "FAIL"
